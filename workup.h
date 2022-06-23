7f02971a5ba7c99a8e037e3cb73a49051e060b9c
celiakassa <celiakassa9@gmail.com>
2022-06-23 14:22:10 +0100

    updating

---
+++ workup.h
@@ -0,0 +1,12 @@
+#define PROJ_ID 256
+
+#define WORKER_TASK2 "./workup"
+#define ID_SIZE 2
+#define WORKERS 4
+#define ARGS_NUM 3
+#define RDV      15
+#define OK 0;
+#define SIGNAL_ERR  -1
+#define ARG_ERR -2
+#define SHM_ERR -3
+#define BUFFER_SIZE 16384
